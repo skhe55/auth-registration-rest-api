86ac2ac8acee335bf08538badc0c1502cc7894ba
skhe55 <flashback2019@mail.ru>
2021-11-03 22:39:50 +0700

    users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,17 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users.model';
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    forwardRef(() => AuthModule)
+  ],
+  exports: [UsersService]
+})
+export class UsersModule { }
