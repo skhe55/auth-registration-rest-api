c1c2693b9e87e50cd2bde81ddddd38c3a76738bd
skhe55 <flashback2019@mail.ru>
2021-11-03 21:50:16 +0700

    create users dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsEmail, IsNotEmpty, Length } from "class-validator";
+
+export class CreateUserDto {
+    @IsString({ message: 'the email field must be a string' })                  // i use decorators IsEmail and IsNotEmpty 
+    @IsEmail({}, { message: 'Incorrect email' })                                // because these decorators basically represent
+    readonly email: string;                                                     // the validation rules for particular fields
+
+    @IsString({ message: 'the password field must be a string' })               // email filed should contain a valid email id
+    @Length(8, 16, { message: 'at least 8 characters + numbers' })              // and the password field should not be empty
+    @IsNotEmpty()
+    readonly password: string;
+}
