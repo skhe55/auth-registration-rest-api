32ceb9a51d76f15c06144d6b0b89d241fcaecc55
skhe55 <flashback2019@mail.ru>
2021-11-03 22:38:36 +0700

    custom decorator - isValidPassword

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { IsString, IsEmail, IsNotEmpty, Length } from "class-validator";
-
+import { IsValidPassword } from "src/decorators/IsValidPassword";
 export class CreateUserDto {
     @IsString({ message: 'the email field must be a string' })                  // i use decorators IsEmail and IsNotEmpty 
     @IsEmail({}, { message: 'Incorrect email' })                                // because these decorators basically represent
@@ -8,5 +8,6 @@ export class CreateUserDto {
     @IsString({ message: 'the password field must be a string' })               // email filed should contain a valid email id
     @Length(8, 16, { message: 'at least 8 characters + numbers' })              // and the password field should not be empty
     @IsNotEmpty()
+    @IsValidPassword({ message: 'password must be numbers and symbols' })
     readonly password: string;
 }
